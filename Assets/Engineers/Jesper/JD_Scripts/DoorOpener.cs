828d85e0579b010bcdb0497bede3fcf6e60f5fa3
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2021-11-29 10:59:08 +0100

    Add first try of mousecursor changes

--- Assets/Engineers/Jesper/JD_Scripts/DoorOpener.cs
+++ Assets/Engineers/Jesper/JD_Scripts/DoorOpener.cs
@@ -1,13 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DoorOpener : MonoBehaviour
 {
+    private bool openable = false;
 
+    public Texture2D lockedTexture;
+    public Texture2D unLockedTexture;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        while (!openable)
+        {
+            
+        }
+    }
+    void OnMouseEnter()
+    {
+        if (!openable)
+        {
+            Cursor.SetCursor(lockedTexture, new Vector2(), CursorMode.Auto);
+        }
+        if (openable)
+        {
+            Cursor.SetCursor(unLockedTexture, new Vector2(), CursorMode.Auto);
+        }
     }
 }
